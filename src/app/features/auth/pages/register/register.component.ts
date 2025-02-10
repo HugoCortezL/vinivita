c93aad24e67a3e3052444d79eeef5dcb62aafe93
Hugo Cortez <hugocortezprog@gmail.com>
2025-02-10 10:06:40 -0300

    registrar funcionando

--- src/app/features/auth/pages/register/register.component.ts
+++ src/app/features/auth/pages/register/register.component.ts
@@ -5,15 +5,26 @@ import { ViniTextFieldInputComponent } from '../../../../shared/components/dls/t
 import { AuthLayoutComponent } from '../../../../shared/layouts/auth-layout/auth-layout.component';
 import { Constants } from '../../../../core/utils/Constants';
 import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UserAuth } from '../../models/UserAuth.model';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterModule, FormsModule, ReactiveFormsModule, AuthLayoutComponent, ViniTextFieldInputComponent, ViniButtonComponent],
+  imports: [
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AuthLayoutComponent,
+    ViniTextFieldInputComponent,
+    ViniButtonComponent
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
   constants = Constants;
+
+  constructor(private authService: AuthService) { }
 
   form = new FormGroup({
     email: new FormControl<string>(''),
@@ -21,6 +32,19 @@ export class RegisterComponent {
     confirmPassword: new FormControl<string>('')
   })
 
-  login() {
+  register() {
+    const user: UserAuth = {
+      email: this.form.controls['email'].value ?? '',
+      password: this.form.controls['password'].value ?? '',
+    };
+
+    this.authService.register(user).subscribe({
+      next: (response) => {
+        console.log('Usuário registrado com sucesso!', response);
+      },
+      error: (error) => {
+        console.error('Erro ao registrar usuário', error);
+      },
+    });
   }
 }
